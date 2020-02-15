4a87cc2812f3c188077c25ae400df0374a835739
Vanessa Huhn <35490743+vanessahuhn@users.noreply.github.com>
2020-02-15 14:08:55 +0100

    Add files via upload

---
+++ 1.2 Walkthrough/Step 3/myApp/webapp/index.js
@@ -0,0 +1,28 @@
+/*Now, we create a new index.js script that will contain the application logic for this tutorial step.
+We do this to avoid having executable code directly in the HTML file for security reasons.
+This script will be called by the index.html. We defined it there as a module in a declarative way.*/
+
+/*Instead of using native JavaScript to display a dialog we want to use a simple SAPUI5 control.
+Controls are used to define appearance and behavior of parts of the screen.
+In the example above, the callback of the init event is where we now instantiate a SAPUI5 text control.
+The name of the control is prefixed by the namespace of its control library sap/m/
+and the options are passed to the constructor with a JavaScript object. For our control we set the text property to the value “Hello World”.
+We chain the constructor call of the control to the standard method placeAt that is used
+to place SAPUI5 controls inside a node of the document object model (DOM) or any other SAPUI5 control instance.
+We pass the ID of a DOM node as an argument. As the target node we use the body tag of the HTML document and give it the ID content.
+All controls of SAPUI5 have a fixed set of properties, aggregations, and associations for configuration.
+You can find their descriptions in the Demo Kit.
+In addition, each controls comes with a set of public functions that you can look up in the API reference.*/
+sap.ui.define([
+	"sap/m/Text"//The name of the control is prefixed by the namespace of its control library sap/m/
+], function (Text) {
+	"use strict";
+
+	/*alert("UI5 is ready");*/
+	new Text({//the options are passed to the constructor with a JavaScript object
+		text: "Hello World"//we set the text property to the value “Hello World”
+	}).placeAt("content");//We chain the constructor call of the control to the standard method placeAt that is used
+						  //to place SAPUI5 controls inside a node of the document object model (DOM) or any other SAPUI5 control instance.
+						  //We pass the ID of a DOM node as an argument.
+						  //As the target node we use the body tag of the HTML document and give it the ID "content".
+});
