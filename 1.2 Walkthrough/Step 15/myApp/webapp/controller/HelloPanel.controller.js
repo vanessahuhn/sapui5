5060cc0fb80247c8f5e6ee037bd06021c0a5f3c6
Vanessa Huhn <35490743+vanessahuhn@users.noreply.github.com>
2020-02-15 14:17:54 +0100

    Add files via upload

---
+++ 1.2 Walkthrough/Step 15/myApp/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,17 @@
+/*To have a reusable asset, the method onShowHello is also moved from the app controller to the HelloPanel controller.*/
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+      onShowHello : function () {
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // show message
+         MessageToast.show(sMsg);
+      }
+   });
+});
